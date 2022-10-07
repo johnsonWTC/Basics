40fc33956bb463fce3fdf111cb6a04ddc0651519
johns <johns@LAPTOP-EKLFFGUE>
2022-10-07 12:05:44 +0200

    Add project files.

---
+++ WebApplication1/Interface/ICalculator.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Interface
+{
+    public interface ICalculator
+    {
+        int AddNumbers(int numA,int numb);
+        int MultiplyNumber(int numA,int numb);
+
+        int SubtractNumber(int numA,int numb);
+
+        int DevideNumber(int numA,int numb);
+    }
+}
